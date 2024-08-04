24e428a75d63e6a275368568ecfd4ad4259517db
Aran <138863634+araaancode@users.noreply.github.com>
2024-08-04 20:07:14 +0330

    Update Image.jsx

--- frontend/src/landing/components/Image.jsx
+++ frontend/src/landing/components/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
   src = src && src.includes('https://')
     ? src
-    : 'http://localhost:5000/uploads/'+src;
+    : 'https://kome-backend.onrender.com/uploads/'+src;
   return (
     <img style={{height:'300px'}} className="w-full" {...rest} src={src} alt={''} />
   );
