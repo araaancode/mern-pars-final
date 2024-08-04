188d50abcb503929563d7142ac6b3c107fd34c44
araaancode <araaancode@gmail.com>
2024-08-04 19:41:56 +0330

    deploy 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const colors = require("colors")
 
 const connectDB = async () => {
-  const conn = await mongoose.connect("mongodb://localhost:27017/mernparsdb")
+  const conn = await mongoose.connect("mongodb+srv://araaancode:2fcmKqD47IJ8q2Q1@kome.sutbc0i.mongodb.net/")
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
 }
